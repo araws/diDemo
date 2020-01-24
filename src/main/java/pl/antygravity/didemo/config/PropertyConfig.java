dafc52bd4b63c4a492c0cea69ee90374d2686876
rawsk <rawski.andrzej@gmail.com>
2020-01-24 11:09:48 +0100

    refactor properties to spring boot specific file

--- src/main/java/pl/antygravity/didemo/config/PropertyConfig.java
+++ src/main/java/pl/antygravity/didemo/config/PropertyConfig.java
@@ -1,21 +1,13 @@
 package pl.antygravity.didemo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 import pl.antygravity.didemo.examplebeans.FakeDataSource;
 import pl.antygravity.didemo.examplebeans.FakeJmsBroker;
 
 @Configuration
-@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
 
     @Value("${anty.user}")
     String user;
@@ -38,7 +30,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -51,11 +43,5 @@ public class PropertyConfig {
         fakeJmsBroker.setPassword(jmsPassword);
         fakeJmsBroker.setUrl(jmsURL);
         return fakeJmsBroker;
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
     }
 }
